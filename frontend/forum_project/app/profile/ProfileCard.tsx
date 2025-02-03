6c8c36d9104ca37557524bfcc48fe79a5bdc0f4c
chiwasushuba <joshua11084@gmail.com>
2025-02-03 15:53:07 +0800

    feat: added profileCard and fixed header in profile page

---
+++ frontend/forum_project/app/profile/ProfileCard.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+interface ProfileInfo {
+  username: string;
+}
+
+export default function ProfileCard({ username }: ProfileInfo){
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>{username}</CardTitle>
+        <CardTitle>Following: 4 Followers: 4</CardTitle>
+        <CardDescription>Bio</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  )
+
+}
