6b02e36e8ac01dc4366a37c25f140b291b94e986
Kellie Kaw <kellie_kaw@dlsu.edu.ph>
2025-02-04 12:44:00 +0800

    feat: add popular and favorites pages; fix: posts in profile and home page

--- frontend/forum_project/app/profile/page.tsx
+++ frontend/forum_project/app/profile/page.tsx
@@ -1,11 +1,37 @@
 'use client'
 
+import { useState } from 'react'
+import Header from '@/components/Header';
+import Post from "@/components/post";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import ProfileCard from './ProfileCard';
 
 export default function Profile() {
 
   return ( 
-    <div >
-      <h1>Profile</h1>
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col md:flex-row w-full">
+        <div className="w-full p-6">
+        <SidebarProvider>
+        <AppSidebar />
+          <SidebarInset>
+          <Header />
+            <div className="flex flex-col gap-4 px-4 py-10">
+              <div className="mx-auto h-full w-full max-w-3xl rounded-xl">
+                <div className="flex flex-col items-center justify-center w-full mt-4 space-y-4">
+                  <ProfileCard username='Joshua'/>
+                  <Post id="6" username="Joshua" title="First Post!" content="First post in the forum" />
+                  <Post id="7" username="Joshua" title="Greetings!" content="Hello" />
+                  <Post id="8" username="Joshua" title="Hello" content="Hi!" />
+                  <Post id="9" username="Joshua" title="Hi" content="woahhh" />
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/forum_project/components/Header.tsx
@@ -0,0 +1,25 @@
+
+import { Github } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export default function Header(){
+
+  return (
+    <div className="flex justify-between items-center w-full px-10 border border-gray-300 rounded-lg">
+      <Github size={64}></Github>
+
+      <div className="flex items-center w-1/2 space-x-2">
+        <Input 
+          type="email" 
+          placeholder="Search 'Website Name'"
+          className="w-full h-12"
+          />
+        <Button type="submit">Search  </Button>
+      </div>
+      
+      <Github size={64}></Github>
+    </div>
+  );
+
+}
