63a2849456fc732ec66e618dc3d372e5557b0c2d
Christian Felipe <chrisfelipe525@gmail.com>
2025-03-05 17:57:38 +0800

    fix: signup and login route working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose')
 
 // login the user
 const loginUser = async (req, res) => {
+    const {email, password} = req.body
 
     try {
+        const user = await User.login(email, password)
+        
+        res.status(200).json({email: user.email, username: user.username})
 
     } catch(error) {
         res.status(400).json({error: error.message})
@@ -17,6 +21,11 @@ const loginUser = async (req, res) => {
 const signUpUser = async (req, res) => {
 
     try {
+        const {email, password, username, firstName, lastName} = req.body
+
+        const user = await User.signup(email, password, username, firstName, lastName);
+
+        res.status(200).json({email})
 
     } catch(error) {
         res.status(400).json({error: error.message})
@@ -30,13 +39,15 @@ const logoutUser = async (req, res) => {
 
     try {
 
+        res.status(200).json({message: 'Logged out successfully'})
+
     } catch(error) {
         res.status(400).json({error: error.message})
     }
 
 }
 
-
+// Do not edit this yet since I will check if we need it
 const getUser = async (req, res) => {
 
     try {
@@ -51,6 +62,20 @@ const getUser = async (req, res) => {
 const deleteUser = async(req, res) => {
 
     try {
+
+        const { id } = req.params
+
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({error:'No such user'})
+        }
+
+        const user = await User.findOneAndDelete({_id: id})
+
+        if(!user) {
+            return res.status(400).json({error:'No such user'})
+        }
+
+        res.status(200).json({message: 'Deleted account successfully'})
 
     } catch(error) {
         res.status(400).json({error: error.message})
