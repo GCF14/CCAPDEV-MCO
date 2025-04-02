03113e61cd1e1fcddee3a53ba5b18c4a43cea96d
Kellie Kaw <kellie_kaw@dlsu.edu.ph>
2025-04-02 23:19:55 +0800

    changed next.config to allow all http and https hosts

--- frontend/forum_project/next.config.ts
+++ frontend/forum_project/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true, // allows SVGs
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allows all https images
+      },
+      {
+        protocol: "http",
+        hostname: "**", // allows all http images 
+      },
+    ],
+  },
 };
 
 export default nextConfig;
